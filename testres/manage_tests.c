2b92a70b254aa52ac983f62c94e41bfc7eadad2d
Sergey Bronnikov <sergeyb@bronevichok.ru>
2018-08-27 18:46:16 +0300

    testres: reorder funcs

--- testres/manage_tests.c
+++ testres/manage_tests.c
@@ -8,14 +8,13 @@ void print_reports(report_t * report);
 void print_suites(suite_t * suite);
 void print_tests(test_t * test);
 
-void delete_tests(test_t * test);
-void delete_suites(suite_t * suite);
 void delete_reports(report_t * report);
+void delete_suites(suite_t * suite);
+void delete_tests(test_t * test);
 
 void push_report(report_t * report, enum format format, suite_t * suite);
 void push_suite(suite_t * suite, char* name, test_t * test, int n_failures, int n_errors);
 void push_test(test_t * test, char* name, char* time, enum test_status status);
-
 
 void print_reports(report_t * report) {
     report_t * current = report;

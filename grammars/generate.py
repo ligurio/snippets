9f1c8293c00e179186c07d1cce81e53df434c39b
Sergey Bronnikov <sergeyb@bronevichok.ru>
2019-03-11 18:31:28 +0300

    update grammars

--- grammars/generate.py
+++ grammars/generate.py
@@ -1,20 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from hypothesis import given
 from hypothesis.strategies import text
 from lark import Lark
 import hypothesis.extra.lark
-import unittest
 import argparse
 
-class TestEncoding(unittest.TestCase):
-        @given(text())
-        def test_decode_inverts_encode(self, s):
-                print(s)
-                m = hypothesis.extra.lark.from_lark("robotstxt.lark", start='hello_world')
+@given(text())
+def generate_tests(self, grammar, start):
+    print(self, grammar, start)
+    m = hypothesis.extra.lark.from_lark(grammar, start=start)
 
 if __name__ == '__main__':
-
-    #parser = lark.Lark(GRAMMAR)
-    #tree = parser.parse("(5 * (3 << x)) + y - 1")
 
     parser = argparse.ArgumentParser(description='Generate grammar samples.')
     parser.add_argument('--grammar', dest='grammar',
@@ -22,7 +20,5 @@ if __name__ == '__main__':
     parser.add_argument('--start', dest='start',
                         help='start terminal')
     args = parser.parse_args()
-    print(args.grammar)
-    #print(args.accumulate(args.integers))
-
-    unittest.main()
+    print(args.grammar, args.start)
+    generate_tests(args.grammar, args.start)

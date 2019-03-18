359bc1b404833564334c6cb461a6ed9cbacccbb7
Sergey Bronnikov <sergeyb@bronevichok.ru>
2019-03-18 18:54:07 +0300

    update grammars

--- grammars/README.md
+++ grammars/README.md
@@ -1,1 +1,7 @@
-Grammars suitable for [lark](https://github.com/lark-parser/lark) parser.
+Grammars suitable for [lark](https://github.com/lark-parser/lark) parser:
+
+- ```mime.lark```: MIME [Media Types](https://www.iana.org/assignments/media-types/media-types.xhtml)
+- ```rfc-1738.lark```: [RFC 1738: Uniform Resource Locators (URL)](https://www.ietf.org/rfc/rfc1738.txt) (WIP)
+- ```palindrome.lark```: palindrome is a word, number, or other sequence of
+characters which reads the same backward as forward, such as madam or racecar
+([issue #1881](https://github.com/HypothesisWorks/hypothesis/issues/1881))
--- grammars/generate.py
+++ grammars/generate.py
@@ -1,16 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from hypothesis import given
-from hypothesis.strategies import text
 from lark import Lark
-import hypothesis.extra.lark
+from hypothesis.extra.lark import from_lark
 import argparse
-
-@given(text())
-def generate_tests(self, grammar, start):
-    print(self, grammar, start)
-    m = hypothesis.extra.lark.from_lark(grammar, start=start)
+import sys
 
 if __name__ == '__main__':
 
@@ -20,5 +14,10 @@ if __name__ == '__main__':
     parser.add_argument('--start', dest='start',
                         help='start terminal')
     args = parser.parse_args()
-    print(args.grammar, args.start)
-    generate_tests(args.grammar, args.start)
+    if (not args.grammar):
+	sys.exit(1)
+    if args.grammar == "mime.lark":
+       sys.setrecursionlimit(10000)
+
+    with open(args.grammar, 'r') as grammar:
+    	print from_lark(Lark(open(args.grammar, 'r'), start="start")).example()

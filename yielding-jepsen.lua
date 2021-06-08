757bf3b50e9fa05250447cf604610d09c6a67e81
Sergey Bronnikov <sergeyb@tarantool.org>
2021-06-08 22:38:39 +0300

    update yielding jepsen

--- yielding-jepsen.lua
+++ yielding-jepsen.lua
@@ -3,20 +3,20 @@
 -- https://gist.github.com/sergos/c2dae39bf1ac47519356de23601ea7f4
 -- https://aphyr.com/posts/316-jepsen-etcd-and-consul
 -- https://luafun.github.io/basic.html
+-- https://github.com/tarantool/p2phub/blob/60a511ac80f376842ac4187bc4decc498b6abf59/src/lib/hashing.lua#L46-L65
 
 local fiber = require('fiber')
-local http = require('http.client').new()
+local clock = require('clock')
 local fun = require('fun')
-local log = require('log')
 local net_box = require('net.box')
 local math = require('math')
 
 math.randomseed(os.time())
 
-local op_channel = fiber.channel(1000)
-local res_channel = fiber.channel(1000)  
-local n_fibers = 10
-local n_ops = 1000
+local n_fibers = 50
+local n_ops = 5 * 1000
+local op_channel = fiber.channel(n_ops)
+local res_channel = fiber.channel(n_ops)
 
 box.cfg{
   listen = 3301,
@@ -25,72 +25,101 @@ box.cfg{
 box.once('schema', function()
    box.schema.space.create('test')
    box.space.test:create_index('primary')
-   box.schema.user.grant('guest', 'read,write,execute', 'universe')
+   box.schema.user.grant('guest', 'read, write, execute', 'universe')
 end)
 
 local function r()
-    return {f = 'read', v = nil}
+  return { type = 'invoke', f = 'read', v = nil }
 end
 
 local function w()
-    return {f = 'write', v = math.random(1, 10)}
+  return { type = 'invoke', f = 'write', v = math.random(1, 10) }
 end
 
 local function cas()
-    return {f = 'cas', v = {math.random(1, 10), math.random(1, 10)}}
+  return {
+    type = 'invoke',
+    f = 'cas',
+    v = {math.random(1, 10), math.random(1, 10)}
+  }
 end
 
-local function client()
-  fiber.sleep(math.random(1, 4))
-  local conn = net_box.connect('127.0.0.1:3301')
-  conn:ping()
-  while true do
-    if op_channel:is_empty() then
-      print('client closes connection')
-      conn:close()
-      return
-    end
-    fiber.yield()
-    local op = op_channel:get()
-    local ok
-    print('client', op.f)
-    if op.f == 'write' then
-      ok = pcall(conn.space.test:replace({1, op.v}))
-      res_channel:put({status = ok, f = 'write'})
-    elseif op.f == 'read' then
-      ok = pcall(conn.space.test:select(1))
-      res_channel:put({status = ok, f = 'read'})
+local function cas_op(id, val1, val2, space)
+  box.begin()
+  local tuple = box.space[space]:get{id}
+  if tuple then
+    if tuple[2] == val1 then
+        box.space[space]:update({id}, {{'=', 2, val2}})
     end
   end
+  box.commit()
+end
+
+local function register_client()
+  local conn = net_box.connect('127.0.0.1:3301')
+  assert(conn:ping(), true)
+  local ok
+  while not op_channel:is_empty() do
+    local op = op_channel:get()
+    res_channel:put({ type = op.type,
+                      f = op.f,
+                      time = clock.time()
+                    })
+    if op.f == 'write' then
+      ok = pcall(conn.space.test:replace({1, op.v}))
+    elseif op.f == 'read' then
+      ok = pcall(conn.space.test:select(1))
+    elseif op.f == 'cas' then
+      ok = cas_op(1, op.v[1], op.v[2], 'test')
+    end
+    res_channel:put({ type = ok,
+                      f = op.f,
+                      time = clock.time()
+                    })
+    fiber.yield()
+  end
+  print('client close connection')
+  conn:close()
 end
 
 local function process_results()
-  -- read results
-  local processed = 0
-  while processed ~= n_ops do
+  while not res_channel:has_writers() do
     local res = res_channel:get()
-    print('res', res.status, res.f)
-    processed = processed + 1
+    local inspect = require('inspect')
+    print(inspect.inspect(res, {newline=' ', indent=''}))
   end
-  print('FIN')
 end
 
 local function main()
   -- generate operations
-  local gen = fun.rands(0, 10):map(function(x) return x == 0 and r or w end):take(n_ops):totable()
-  for _, op in pairs(gen) do
-    local op = op()
-    op_channel:put(op)
-    print('gen operation', op.f)
+  local gen = fun.rands(0, 3):map(function(x)
+                                  return (x == 0 and r) or
+                                         (x == 1 and w) or
+                                         (x == 2 and cas) end):take(n_ops):totable()
+  for _, operation in pairs(gen) do
+    op_channel:put(operation())
   end
 
   -- start clients
-  for i = 1, n_fibers, 1 do
-    fiber.create(client)
+  for n = 1, n_fibers do
+    print(string.format('start %dth client', n))
+    fiber.create(register_client)
   end
 
   -- start processing
-  fiber.create(process_results)
+  local f = fiber.create(process_results)
+  f:set_joinable(true)
+  f:join()
+
+  -- teardown
+  if op_channel:is_empty() and
+     not op_channel:has_readers() then
+    op_channel:close()
+  end
+  if res_channel:is_empty() and
+     not res_channel:has_writers() then
+    res_channel:close()
+  end
 end
 
 main()

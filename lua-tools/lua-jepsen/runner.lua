5033fffe016ea3d732abbeade833d7df768ed29b
Sergey Bronnikov <sergeyb@tarantool.org>
2021-09-08 11:40:34 +0300

    fix errors to make luacheck happy

--- lua-tools/lua-jepsen/runner.lua
+++ lua-tools/lua-jepsen/runner.lua
@@ -1,8 +1,7 @@
-local register_workload = require('register')
 local fun = require('fun')
 local inspect = require('inspect')
 
 local g2 = fun.range(5):take(3)
 for a, b in g2 do
-    print(inspect.inspect(a), inspect.inspect(b), inspect.inspect(c))
+    print(inspect.inspect(a), inspect.inspect(b))
 end
